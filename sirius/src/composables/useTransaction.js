094349c46fdba00fb10f300db6d7744299f87f0b
Heuder Rodrigues de Sena <documento_arquivo@hotmail.com>
2023-03-24 10:38:56 -0300

    refatorando a função que busca todas as transações do usuário

---
+++ sirius/src/composables/useTransaction.js
@@ -0,0 +1,10 @@
+export const useTransaction = async () => {
+
+
+
+
+    return {
+
+    }
+
+}
--- sirius/src/views/HomeView.vue
+++ sirius/src/views/HomeView.vue
@@ -1,39 +1,27 @@
 <template>
   <div>
     <InformationHeadComponent msg="Lista de transações" />
-    <span v-if="loading">carregando...</span>
-    <span v-if="!load_transaction">...</span>
-    <TableComponent v-if="loading === false" :transaction="load_transaction" />
+    <template v-if="isLoading">Carregando... </template>
+    <template v-else>
+        <TableComponent :transaction="transaction" />
+    </template>
   </div>
 </template>
 
 <script setup>
-import { computed, onMounted, ref } from "vue";
+import { useAsyncState  } from "@vueuse/core"
 import TableComponent from "../components/TableComponent.vue";
 import InformationHeadComponent from "../components/InformationHeadComponent.vue";
 import ExempleComponent from "../components/ExempleComponent.vue";
-// TODO
 
 import api from "../plugins/axios";
+import {useTransaction} from "../composables/useTransaction";
 
-const load_transaction = ref({});
-const loading = ref(true);
+// TODO
+const {state:transaction, isLoading} = useAsyncState(api.post("/transaction").then(t=>t.data[0].Transaction),[],{resetOnExecute:false})
 
-onMounted(() => {
-  api.post("/transaction").then((response) => {
-    load_transaction.value = response.data?.[0].Transaction;
-  });
-  loading.value = false;
-  console.log(load_transaction);
-});
 
-const total_money = computed(() => {
-  load_transaction[0].balance.reduce(function (soma, i) {
-    return soma++;
-  });
-});
 
-console.log(total_money);
 </script>
 
 
